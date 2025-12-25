76dcb9fef476af1119996b13698339c4397afb3d
git <ryanmoralesaz@gmail.com>
2025-12-25 02:25:34 +0000

    trying to update tmux

--- init.lua
+++ init.lua
@@ -260,7 +260,17 @@ vim.api.nvim_create_autocmd("VimEnter", {
 })
 
 -- Clipboard settings - works for both local and SSH
-vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
 
 vim.opt.foldcolumn = "2"
 
